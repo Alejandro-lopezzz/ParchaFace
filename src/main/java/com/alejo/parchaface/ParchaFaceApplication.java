7246051a85b50b07d5c10d81a8333c159665dbd2
Alejandro-lopezzz <xarteagax123@gmail.com>
2025-12-12 15:44:58 -0500

    Primer Commit, subiendo esqueleto

---
+++ src/main/java/com/alejo/parchaface/ParchaFaceApplication.java
@@ -0,0 +1,13 @@
+package com.alejo.parchaface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParchaFaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParchaFaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alejo/parchaface/ParchaFaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejo.parchaface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParchaFaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
